7386bd3bf7bd48914b4cf4268bd6cee47ecc8310
HasanAbbadi <hasanabady55@gmail.com>
2023-06-01 20:40:01 +0300

    functionality implemented, styles remain

---
+++ app.js
@@ -0,0 +1,56 @@
+const displayElement = document.querySelector(".display")
+const secondsInput = document.getElementById("seconds-input")
+const minutesInput = document.getElementById("minutes-input")
+
+let isPaused = true;
+let interval;
+
+
+const activate = () => {
+  secondsInput.setAttribute('readonly', `${isPaused}`)
+  minutesInput.setAttribute('readonly', `${isPaused}`)
+
+  if (isPaused) {
+    isPaused = false
+
+    let mins = minutesInput.value
+    let secs = secondsInput.value
+    duration = (mins * 60) + parseInt(secs)
+
+    startTimer(duration)
+  } else {
+    isPaused = true
+    clearInterval(interval)
+    displayElement.classList.remove("active")
+  }
+}
+
+const convertTime = (time) => {
+  const minutes = Math.floor(time / 60)
+  const seconds = time - (minutes * 60)
+  return { minutes, seconds }
+}
+
+const startTimer = (duration) => {
+  if (isPaused) return;
+
+  interval = setInterval(() => {
+    duration -= 1
+    const { minutes, seconds } = convertTime(duration)
+    displayElement.classList.add("active")
+    minutesInput.value = minutes
+    secondsInput.value = seconds
+  }, 1000)
+}
+
+document.addEventListener('keypress', event => {
+  if (event.key == " ") {
+    activate()
+  } else if (event.key == "r") {
+    minutesInput.value = "25"
+    secondsInput.value = "00"
+  } else if (event.key == "b") {
+    minutesInput.value = "05"
+    secondsInput.value = "00"
+  } 
+})
